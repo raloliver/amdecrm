50a46535f3ce0139135bb465811850e1cbb07a1a
Israel Oliveira <raloliver.com@gmail.com>
2021-04-26 21:11:42 +0100

    init amdecrm

---
+++ src/main/java/br/com/raloliver/amdecrm/App.java
@@ -0,0 +1,13 @@
+package br.com.raloliver.amdecrm;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
