c2794326de7b7dddb73d098179c108b725d33d71
Israel Oliveira <raloliver.com@gmail.com>
2021-06-12 09:10:01 +0100

    Model and ModelMap to pass data from controler to view

--- src/main/java/br/com/raloliver/amdecrm/controller/HomeController.java
+++ src/main/java/br/com/raloliver/amdecrm/controller/HomeController.java
@@ -1,9 +1,9 @@
 package br.com.raloliver.amdecrm.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 @RequestMapping("/")
@@ -13,6 +13,17 @@ public class HomeController {
     @GetMapping("/")
     public String home() {
         return "home";
+    }
+
+    /**
+     * Para passar dados da controler para view, podemos utilizar a interface model
+     * e a classe model mapping Model X ModelMap: Model é a interface, e o ModelMap
+     * é a implementação da interface Model.
+     */
+    @GetMapping("/mensagem")
+    public String mensagem(ModelMap modelMap) {
+        modelMap.addAttribute("mensagem", "Carregando...");
+        return "mensagem";
     }
 
 }
