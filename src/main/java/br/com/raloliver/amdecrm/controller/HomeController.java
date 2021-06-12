2ae5f18f6c5d523ca67eb2bba7337aef02610860
Israel Oliveira <raloliver.com@gmail.com>
2021-06-12 09:31:10 +0100

    Pass data from view to controler with PathVariable and RequestParam

--- src/main/java/br/com/raloliver/amdecrm/controller/HomeController.java
+++ src/main/java/br/com/raloliver/amdecrm/controller/HomeController.java
@@ -3,7 +3,9 @@ package br.com.raloliver.amdecrm.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -37,6 +39,21 @@ public class HomeController {
 
         modelAndView.setViewName("mensagem");
         modelAndView.addObject("mensagem", "Carregando...");
+
+        return modelAndView;
+    }
+
+    /**
+     * PathVariable: exibe dados a partir de um caminho na url
+     * (@GetMapping("/saudacao/{nome}") | @PathVariable String nome). RequestParam:
+     * exibe dados a partir de um query na url
+     */
+    @GetMapping("/saudacao")
+    public ModelAndView saudacao(@RequestParam(required = false, defaultValue = "João") String nome) {
+        ModelAndView modelAndView = new ModelAndView();
+
+        modelAndView.setViewName("saudacao");
+        modelAndView.addObject("nome", nome);
 
         return modelAndView;
     }
